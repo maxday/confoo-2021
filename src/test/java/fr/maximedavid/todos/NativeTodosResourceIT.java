d8d08c504ab0fdcdef4fe92956bb5a032593ed59
Maxime DAVID <contact@maximedavid.fr>
2021-02-24 07:24:10 -0500

    firstCommit

---
+++ src/test/java/fr/maximedavid/todos/NativeTodosResourceIT.java
@@ -0,0 +1,9 @@
+package fr.maximedavid.todos;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTodosResourceIT extends TodosResourceTest {
+
+    // Execute the same tests but in native mode.
+}
